c52357efc86dd9ad2756681136b2d1d39ca2e89f
sashaivanovaPro <sashaivanova.pro@gmail.com>
2023-02-14 12:31:54 +0300

    add primary solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+  matrix.forEach(function (item, i) {
+    if (i % 2 !== 0){
+      item.reverse();
+   }
+    arr = matrix.flat(Infinity);
+  })
+  return(arr);
+  }
+
+/* один массив из нескольких в порядке возрастания
+function mergeArrays(arr1, arr2) {
+  return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
 }
+*/
+/* Для неопределенной матрицы
+if (paramName === undefined) */
